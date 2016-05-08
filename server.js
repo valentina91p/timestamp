4e4ff32da7cece52b8c8b05fc6a1b9702ac7efeb
c-valentinamm <c.valentinamm@gmail.com>
2016-05-08 00:03:40 -0400

    Added landing page, moment function replaced by utc()

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 var app = require("express")();
 var moment = require("moment");
-var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul",
-	"Aug","Sep","Oct","Nov","Dec"];
 
-function isDate(d){
-	return !isNaN(new Date(d).valueOf());
-}
+app.get("/",function(req,res){
+	res.sendFile("index.html", {root: __dirname});
+});
 
 app.get("/:d", function(req,res){
 	var parsed_dates = {
 		unix: null,
 		natural: null
 	};
-	console.log("params: "+req.params.d);
 	if(/^-?\d+$/.test(req.params.d))
 		var date = moment.unix(Number(req.params.d));
-	else{
-		console.log("aqui");
-		var date = moment(req.params.d);
-	}
+	else
+		var date = moment.utc(req.params.d);
 	
 	if(date.isValid()){
-		parsed_dates.unix = date.unix();
-		parsed_dates.natural = date.utc().format("MMMM D, YYYY");
+		parsed_dates.unix = date.utc().unix();
+		parsed_dates.natural = date.format("MMMM D, YYYY");
 	}
 	res.json(parsed_dates);
 });
